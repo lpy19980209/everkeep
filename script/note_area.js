47be92839cab91cbf62dcb6a960d9cbab9213c87
lpy <lpy19980209@gmail.com>
2018-10-11 19:00:51 +0800

    去边框、加标题等编辑器美化

--- script/note_area.js
+++ script/note_area.js
@@ -1,12 +1,13 @@
 
 tinymce.init({
-    selector: '#mytextarea',
+    selector: '#my_text_area',
     theme: 'modern',
-    // inline: true,
+    inline: true,
     menubar: false,
     language: 'zh_CN',
     plugins: 'fileupload, link, preview, table, hr, textcolor, lists, autolink',
     toolbar: 'fontselect | fontsizeselect | forecolor textcolor bold italic underline strikethrough | ' +
         'bullist numlist | blockquote link fileupload | table hr | align indent outdent | ' +
         'subscript superscript removeformat ',
+    fixed_toolbar_container: "#mce_toolbar",
 });
