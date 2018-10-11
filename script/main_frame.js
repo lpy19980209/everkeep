2bd253c363449774602a7204405cbfc52a202ae0
lpy <lpy19980209@gmail.com>
2018-10-11 14:41:05 +0800

    编辑器外观调整

--- script/main_frame.js
+++ script/main_frame.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
     $("#img8").click(function () {
         if($('#slide1').css('left') == '-450px')
         {
-        $("#slide2").hide().css({"left":"-450px"});
+        // $("#slide2").hide().css({"left":"-450px"});
         $("#slide3").hide().css({"left":"-450px"});
         $("#slide4").hide().css({"left":"-450px"});
         $("#slide1").show();
@@ -56,28 +56,28 @@ $(document).ready(function(){
         }
     })
 })
-$(document).ready(function () {
-    $("#img2").click(function () {
-        if($('#slide2').css('left') == '-450px')
-        {
-            $("#slide1").hide().css({"left": "-450px"});
-            $("#slide3").hide().css({"left": "-450px"});
-            $("#slide4").hide().css({"left": "-450px"});
-            $("#slide2").show();
-            $("#slide2").animate({left: 0}, "slow");
-        }
-        else
-        {
-            $("#slide2").animate({left:-450},"slow");
-        }
-    })
-})
+// $(document).ready(function () {
+//     $("#img2").click(function () {
+//         if($('#slide2').css('left') == '-450px')
+//         {
+//             $("#slide1").hide().css({"left": "-450px"});
+//             $("#slide3").hide().css({"left": "-450px"});
+//             $("#slide4").hide().css({"left": "-450px"});
+//             $("#slide2").show();
+//             $("#slide2").animate({left: 0}, "slow");
+//         }
+//         else
+//         {
+//             $("#slide2").animate({left:-450},"slow");
+//         }
+//     })
+// })
 $(document).ready(function () {
     $("#img3").click(function () {
         if($('#slide3').css('left') == '-450px')
         {
         $("#slide1").hide().css({"left": "-450px"});
-        $("#slide2").hide().css({"left": "-450px"});
+        // $("#slide2").hide().css({"left": "-450px"});
         $("#slide4").hide().css({"left": "-450px"});
         $("#slide3").show();
         $("#slide3").animate({left: 0}, "slow");
@@ -93,7 +93,7 @@ $(document).ready(function () {
         if($('#slide4').css('left') == '-450px')
         {
         $("#slide1").hide().css({"left": "-450px"});
-        $("#slide2").hide().css({"left": "-450px"});
+        // $("#slide2").hide().css({"left": "-450px"});
         $("#slide3").hide().css({"left": "-450px"});
         $("#slide4").show();
         $("#slide4").animate({left: 0}, "slow");
