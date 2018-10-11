a9798be1e51809b1d090f3518a336d9d15ded776
lpy <lpy19980209@gmail.com>
2018-10-11 13:42:04 +0800

    编辑器嵌入主页面以及资源路径调整

---
+++ script/note_area.js
@@ -0,0 +1,11 @@
+
+tinymce.init({
+    selector: '#mytextarea',
+    theme: 'modern',
+    menubar: false,
+    language: 'zh_CN',
+    plugins: 'fileupload, link, preview, table, hr, textcolor, lists, autolink',
+    toolbar: 'fontselect | fontsizeselect | forecolor textcolor bold italic underline strikethrough | ' +
+        'bullist numlist | blockquote link fileupload | table hr | align indent outdent | ' +
+        'subscript superscript removeformat ',
+});
