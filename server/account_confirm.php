7734dc315987dbeed14ef94ec3782f3e1f19368a
lpy <lpy19980209@gmail.com>
2018-10-20 23:15:37 +0800

    添加了两个后端脚本，打算做邮箱验证

---
+++ server/account_confirm.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liupy
+ * Date: 2018/10/20
+ * Time: 22:16
+ */
+
+require_once __DIR__ . "/response_code.php";
+
+//验证数据完整性
+if(!isset($_GET['userid']) || !isset($_GET['code']))
+{
+    $msg = json_encode([
+        "code" => GET_PARM_ERROR,
+        "msg" => "GET数据不完整",
+    ]);
+    die($msg);
+}
+
+accountConfirm($_GET['userid'], $_GET['code']);
+
+function accountConfirm($userid, $code)
+{
+    $servername = "localhost";
+    $username = "everkeep";
+    $password = "everkeep_team10";
+    $dbname = "everkeep";
+    $tablename = "confirm";
+
+// 创建连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+    if ($conn->connect_error) {
+        $msg = json_encode([
+            "code" => DB_CONNECTION_ERROR,
+            "msg" => "数据库连接失败",
+        ]);
+        die($msg);
+    }
+
+    $sql = <<<EOF
+select * from $tablename where userid = '$userid' and confirmCode = '$code'
+EOF;
+
+    $result = $conn->query($sql);
+    if($result->num_rows > 0) {
+
+    }
+
+    else {
+        $sql = <<<EOF
+select userid from $tablename where userid = '$userid'
+EOF;
+
+        $result = $conn->query($sql);
+        if($result->num_rows > 0) {
+            $msg = json_encode([
+                "code" => CONFIRM_CODE_EXPIRE,
+                "msg" => "信息已过期",
+            ]);
+            die($msg);
+        }
+        else {
+            $msg = json_encode([
+                "code" => USER_NOT_EXIST,
+                "msg" => "用户不存在",
+            ]);
+            die($msg);
+        }
+    }
+    $conn->close();
+}
+
+function alterIsConfirm($userid)
+{
+    $servername = "localhost";
+    $username = "everkeep";
+    $password = "everkeep_team10";
+    $dbname = "everkeep";
+    $tablename = "confirm";
+
+// 创建连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+    if ($conn->connect_error) {
+        $msg = json_encode([
+            "code" => DB_CONNECTION_ERROR,
+            "msg" => "数据库连接失败",
+        ]);
+        die($msg);
+    }
+
+    $sql = <<<EOF
+update $tablename set isConfirm = 1
+ where userid = '$userid'
+EOF;
+
+    if ($conn->query($sql) === TRUE) {
+
+        $msg = json_encode([
+            "code" => SUCCESS,
+            "msg" => "success",
+        ]);
+        die($msg);
+    } else {
+        $msg = json_encode([
+            "code" => ACCOUNT_CONFIRM_ERROR,
+            "msg" => "数据更新失败: " . $conn->error,
+        ]);
+        die($msg);
+    }
+
+    $conn->close();
+}
---
+++ server/confirm_mail.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Liupy
+ * Date: 2018/10/20
+ * Time: 13:37
+ */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../lib/php/PHPMailer/Exception.php';
+require '../lib/php/PHPMailer/PHPMailer.php';
+require '../lib/php/PHPMailer/SMTP.php';
+
+
+function sendAccountConfirm($to, $userid, $code) {
+    $mail = new PHPMailer(true);
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.zoho.com.cn';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'everkeep@zoho.com.cn';                 // SMTP username
+        $mail->Password = 'everkeep_team10';                           // SMTP password
+        $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 465;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom('everkeep@zoho.com.cn', 'EverKeep');
+        $mail->addAddress($to, 'User');     // Add a recipient
+        $mail->addReplyTo('everkeep@zoho.com.cn', 'EverKeep');
+        $mail->addBCC('1315649917@qq.com');
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'EverKeep';
+        $mail->Body    = "点击以下链接确认注册<br> <a href='http://localhost/team10/server/account_confirm.php?userid={$userid}&code={$code}'>EverKeep</a>";
+        $mail->AltBody = "在浏览器打开以下链接确认注册\n http://localhost/team10/server/account_confirm.php?userid={$userid}&code={$code}";
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        return false;
+    }
+}
