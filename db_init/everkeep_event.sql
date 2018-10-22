b7cbed034df587d58c3b321dcff1ace0d4534ee6
lpy <lpy19980209@gmail.com>
2018-10-22 22:40:09 +0800

    添加了几个事务，用于定期清除过期信息，详细信息见/db_init/readme.md

---
+++ db_init/everkeep_event.sql
@@ -0,0 +1,19 @@
+create event everkeep_clear_password_reset_code_outdate
+on schedule
+every 1 second
+do delete from confirm where `usage`=2 and current_timestamp-applyTime>00000000003000;
+
+create event everkeep_clear_account_confirm_code_outdate
+on schedule
+every 1 second
+do delete from confirm where `usage`=1 and current_timestamp-applyTime>00000000235959;
+
+create event everkeep_clear_user_outdate
+on schedule
+every 1 second
+do delete from user where current_timestamp-createTime>00000000240000 and isConfirm=0;
+
+create event everkeep_clear_note_outdate
+on schedule
+every 1 second
+do delete from note where current_timestamp-updateTime>00000030000000 and isDelete=1;
